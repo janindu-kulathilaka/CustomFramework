dcb2630e0723baa50b2793c814c820c0abca4ce4
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-10-28 05:29:03 +0530

    setup routing

---
+++ app/controllers/Pages.php
@@ -0,0 +1,17 @@
+<?php
+class Pages extends Controller
+{
+  public function __construct()
+  {
+  }
+
+  public function index()
+  {
+    $this->view('hello');
+  }
+
+  public function about()
+  {
+    echo "This is about";
+  }
+}
